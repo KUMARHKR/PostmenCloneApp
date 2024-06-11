696ccd2d56ca9983181decde4eca0d3f489d4141
KUMARHKR <dipkumardhawa020@gmail.com>
2024-06-11 11:27:07 +0530

    Updated UI and added new methods in Dashbord.cs
    In `Dashbord.Designer.cs`, new UI elements were added and the form size was adjusted. The `Dashbord.cs` file saw a namespace modification, addition of a new constructor, and two new methods: `callApi_Click` and `resultsText_TextChanged`. Metadata for `statusStrip.TrayLocation` and `$this.TrayHeight` was added in `Dashbord.resx`.

--- PostmenCloneUi/Dashbord.Designer.cs
+++ PostmenCloneUi/Dashbord.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            PostmanClone = new Label();
+            Apilabel = new Label();
+            apiText = new TextBox();
+            callApi = new Button();
+            resultsText = new TextBox();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // PostmanClone
+            // 
+            PostmanClone.AutoSize = true;
+            PostmanClone.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PostmanClone.Location = new Point(152, 82);
+            PostmanClone.Name = "PostmanClone";
+            PostmanClone.Size = new Size(254, 47);
+            PostmanClone.TabIndex = 0;
+            PostmanClone.Text = "Postman Clone";
+            // 
+            // Apilabel
+            // 
+            Apilabel.AutoSize = true;
+            Apilabel.Location = new Point(152, 173);
+            Apilabel.Name = "Apilabel";
+            Apilabel.Size = new Size(60, 32);
+            Apilabel.TabIndex = 1;
+            Apilabel.Text = "API :";
+            // 
+            // apiText
+            // 
+            apiText.Location = new Point(228, 173);
+            apiText.Name = "apiText";
+            apiText.Size = new Size(659, 39);
+            apiText.TabIndex = 2;
+            // 
+            // callApi
+            // 
+            callApi.Location = new Point(951, 173);
+            callApi.Name = "callApi";
+            callApi.Size = new Size(165, 39);
+            callApi.TabIndex = 3;
+            callApi.Text = "Go";
+            callApi.UseVisualStyleBackColor = true;
+            callApi.Click += callApi_Click;
+            // 
+            // resultsText
+            // 
+            resultsText.BackColor = Color.White;
+            resultsText.BorderStyle = BorderStyle.FixedSingle;
+            resultsText.Location = new Point(152, 276);
+            resultsText.Multiline = true;
+            resultsText.Name = "resultsText";
+            resultsText.ReadOnly = true;
+            resultsText.ScrollBars = ScrollBars.Both;
+            resultsText.Size = new Size(964, 309);
+            resultsText.TabIndex = 4;
+            resultsText.TextChanged += resultsText_TextChanged;
+            // 
+            // statusStrip
+            // 
+            statusStrip.BackColor = Color.White;
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 628);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1220, 30);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "systemStatus";
+            // 
+            // systemStatus
+            // 
+            systemStatus.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(60, 25);
+            systemStatus.Text = "Rrady";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Location = new Point(152, 230);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(88, 32);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results";
             // 
             // Dashbord
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1924, 1061);
+            BackColor = Color.White;
+            ClientSize = new Size(1220, 658);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(resultsText);
+            Controls.Add(callApi);
+            Controls.Add(apiText);
+            Controls.Add(Apilabel);
+            Controls.Add(PostmanClone);
             Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(5, 5, 5, 5);
+            Margin = new Padding(5);
             Name = "Dashbord";
             Text = "Postmen Clone";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label PostmanClone;
+        private Label Apilabel;
+        private TextBox apiText;
+        private Button callApi;
+        private TextBox resultsText;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel systemStatus;
     }
 }
--- PostmenCloneUi/Dashbord.cs
+++ PostmenCloneUi/Dashbord.cs
@@ -1,10 +1,35 @@
-namespace PostmenCloneUi
+namespace PostmenCloneUi;
+
+public partial class Dashbord : Form
 {
-    public partial class Dashbord : Form
+    public Dashbord()
     {
-        public Dashbord()
+        InitializeComponent();
+    }
+
+    private async void callApi_Click(object sender, EventArgs e)
+    {
+
+        //Validate input API key
+
+        try
         {
-            InitializeComponent();
+            systemStatus.Text = "Calling API ...";
+
+            // Sample code  - Replace with api call code
+            await Task.Delay(2000);
+
+            systemStatus.Text = "Ready";
         }
+        catch (Exception ex)
+        {
+            resultsLabel.Text = "Error : " + ex.Message;
+            systemStatus.Text = "Error";
+        }
+    }
+
+    private void resultsText_TextChanged(object sender, EventArgs e)
+    {
+
     }
 }
